60c42a75e5506bc35478b78bc52c8987726adee9
blagarazvan <razvanblaga10@gmail.com>
2025-01-11 16:38:03 +0200

    second commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js";
@@ -17,7 +18,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+const __dirname=path.resolve()
 
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
@@ -30,6 +31,13 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 
+if(process.env.NODE_ENV==="production"){
+	app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+	app.get("*",(req,res)=>{
+		res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+	})
+}
 
 app.listen(PORT, () => {
 	console.log("Server is running on http://localhost:" + PORT);
